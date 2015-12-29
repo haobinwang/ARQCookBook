ec0cbf6f53f62b835a7447ea41c183ebd26a279c
Richard_R_Chen <richardr_chen@trend.com.tw>
2015-12-29 11:21:20 +0800

    no message

---
+++ ARQ3CookBook/ARQ3/recipe/deploy.rb
@@ -0,0 +1,36 @@
+include_recipe 'deploy'
+
+node[:deploy].each do |application, deploy|
+  # determine root folder of new app deployment
+  app_root = "#{deploy[:deploy_to]}/current"
+  
+  # deploy init 
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+ 
+  Chef::Log.info("deploy #{application}")
+
+  # install
+  bash 'deploy-rpm' do
+        user 'root'
+        code <<-EOH
+                rpm -ivh "#{app_root}/archive"
+        EOH
+        notifies :start, "service[rpm]", :delayed
+  end
+  
+  # start
+  service 'rpm' do
+        service_name "#{application}"
+        action :nothing
+  end
+  
+end
