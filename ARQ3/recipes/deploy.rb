90963f182e056c028f01bdbdec27a166b03e735c
Richard_R_Chen <richardr_chen@trend.com.tw>
2015-12-31 17:04:31 +0800

    add setup.rb

--- ARQ3/recipes/deploy.rb
+++ ARQ3/recipes/deploy.rb
@@ -1,20 +1,5 @@
 include_recipe 'deploy'
 
-#account for arq
-user 'rbldnsd' do
-  home '/home/rbldnsd'
-end
-
-#arq dependence
-yum_package 'python26' do
-  action :install
-end
-
-#arq dependence
-yum_package 'python-netifaces' do
-  action :install
-end
-
 node[:deploy].each do |application, deploy|
   # determine root folder of new app deployment
   app_root = "#{deploy[:deploy_to]}/current"
---
+++ ARQ3/recipes/setup.rb
@@ -0,0 +1,16 @@
+include_recipe 'deploy'
+
+#account for arq
+user 'rbldnsd' do
+  home '/home/rbldnsd'
+end
+
+#arq dependence
+yum_package 'python26' do
+  action :install
+end
+
+#arq dependence
+yum_package 'python-netifaces' do
+  action :install
+end
