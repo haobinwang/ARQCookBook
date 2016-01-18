ae2bfe182ed4006089d40e937ed4791b77319082
Richard_R_Chen <richardr_chen@trend.com.tw>
2016-01-18 18:15:37 +0800

    check eip if in a record

--- ARQ3/recipes/inst_eip2r53.rb
+++ ARQ3/recipes/inst_eip2r53.rb
@@ -53,32 +53,60 @@ record_set_identifier='awstestarq-ns1'
 record_region=node["opsworks"]["instance"]["region"]
 record_ttl=3600
 RData1=public_ip
+res_records=[{value: RData1}]
 
-resp = r53.change_resource_record_sets({
-  hosted_zone_id: HostedZoneID,
-  change_batch: {
-    comment: "ResourceDescription",
-    changes: [
-      {
-        action: "CREATE", # required, accepts CREATE, DELETE, UPSERT
-        resource_record_set: { # required
-          name: DNSName, # required
-          type: record_type, # required, accepts SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA
-          set_identifier: record_set_identifier,
-          region: record_region, # accepts us-east-1, us-west-1, us-west-2, eu-west-1, eu-central-1, ap-southeast-1, ap-southeast-2, ap-northeast-1, sa-east-1, cn-north-1
-          ttl: record_ttl,
-          resource_records: [
-               {value: RData1}
-          ]
-        },
+
+#try to get record set
+
+resp = r53.list_resource_record_sets({
+    hosted_zone_id:HostedZoneID
+    start_record_name: DNSName,
+    start_record_type: record_type,
+    start_record_identifier: record_set_identifier,
+    max_items: 1})
+
+bNeedUpdate = ture
+resp.resource_record_sets.each do |record|
+   if record.respond_to?(:resource_records)
+      record.resource_records.each do |resource_record|
+         if resource_record.respond_to?(:value)
+            if resource_record.value == public_ip
+                bNeedUpdate = false
+            else
+                res_records.insert(-1,resource_record)
+            end
+            Chef::Log.info("##    value: #{resource_record.value}    ##")
+         end
+      end
+   end
+end
+
+if bNeedUpdate
+    resp = r53.change_resource_record_sets({
+      hosted_zone_id: HostedZoneID,
+      change_batch: {
+        comment: "ResourceDescription",
+        changes: [
+          {
+            action: "CREATE", # required, accepts CREATE, DELETE, UPSERT
+            resource_record_set: { # required
+              name: DNSName, # required
+              type: record_type, # required, accepts SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA
+              set_identifier: record_set_identifier,
+              region: record_region, # accepts us-east-1, us-west-1, us-west-2, eu-west-1, eu-central-1, ap-southeast-1, ap-southeast-2, ap-northeast-1, sa-east-1, cn-north-1
+              ttl: record_ttl,
+              resource_records: res_records
+              ]
+            },
+          },
+        ],
       },
-    ],
-  },
-})
-
-Chef::Log.info("###   Try to add: #{public_ip}   ###")
-Chef::Log.info("###   id: #{resp.change_info.id}   ###")
-Chef::Log.info("###   status: #{resp.change_info.status}   ###")
-Chef::Log.info("###   submitted_at: #{resp.change_info.submitted_at}   ###")
-Chef::Log.info("###   comment: #{resp.change_info.comment}   ###")
-
+    })
+    Chef::Log.info("###   Try to add: #{public_ip}   ###")
+    Chef::Log.info("###   id: #{resp.change_info.id}   ###")
+    Chef::Log.info("###   status: #{resp.change_info.status}   ###")
+    Chef::Log.info("###   submitted_at: #{resp.change_info.submitted_at}   ###")
+    Chef::Log.info("###   comment: #{resp.change_info.comment}   ###")
+else
+    Chef::Log.info("###   #{public_ip} already in record  ###")
+end
