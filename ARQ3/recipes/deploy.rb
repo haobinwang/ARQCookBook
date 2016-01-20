63ee37e37f2896fdda56b74b1754f65c5ec1af45
Richard_R_Chen <richardr_chen@trend.com.tw>
2016-01-20 14:40:19 +0800

    modify 'deloy' to 'config'

--- ARQ3/recipes/deploy.rb
+++ ARQ3/recipes/deploy.rb
@@ -1,18 +1,18 @@
 include_recipe 'deploy'
 
-node[:deploy].each do |application, deploy|
+node[:deploy].each do |application, config|
   # determine root folder of new app deployment
-  app_root = "#{deploy[:deploy_to]}/current"
+  app_root = "#{config[:deploy_to]}/current"
   
   # deploy init 
   opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
+    user config[:user]
+    group config[:group]
+    path config[:deploy_to]
   end
 
   opsworks_deploy do
-    deploy_data deploy
+    deploy_data config
     app application
   end
 
@@ -20,17 +20,18 @@ node[:deploy].each do |application, deploy|
   # rpm install 
   rpm_package "#{application}" do
     source "#{app_root}/archive"
-    notifies :start, "service[rpm]", :delayed
+    notifies :start, "service[#{application}]", :delayed
     action :install
   end
 
   # start
-  service 'rpm' do
-        service_name "#{application}"
-        action :nothing
+  service "#{application}" do
+    #service_name "#{application}"
+    action :nothing
   end
-
 end
+
+
 #include_recipe 'ARQ3::instanceAttributes'
 #include_recipe 'ARQ3::inst_eip2r53'
 #include_recipe 'ARQ3::opsworks_eip'
