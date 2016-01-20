d6e807b5a123da07f84bd24f6b7cb937904a7af6
charles_wang <charles_wang@trend.com.tw>
2016-01-20 17:01:32 +0800

    add ip mapping in attribute file

---
+++ ARQ3/attributes/default.rb
@@ -0,0 +1,4 @@
+default['ip_mapping']['us-east-1']['arq01']['ip']="52.70.181.6"
+default['ip_mapping']['us-east-1']['arq01']['ns']="ns1.awstest.ers.trendmicro.com"
+default['ip_mapping']['us-east-1']['arq02']['ip']="52.71.243.66"
+default['ip_mapping']['us-east-1']['arq02']['ns']="ns2.awstest.ers.trendmicro.com"
