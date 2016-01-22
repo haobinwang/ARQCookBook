676a7c8e136faf85cae574b622980cb7f4cbb8d3
Richard_R_Chen <richardr_chen@trend.com.tw>
2016-01-22 20:02:29 +0800

    1. use subfunction 2. get hosted zone id by name

--- ARQ3/recipes/inst_eip2r53.rb
+++ ARQ3/recipes/inst_eip2r53.rb
@@ -4,118 +4,150 @@ require 'aws-sdk'
 region = node["opsworks"]["instance"]["region"]
 hostname = node["opsworks"]["instance"]["hostname"]
 
-eip_new = node["ip_mapping"][region][hostname]["ip"]
+public_ip = node["ip_mapping"][region][hostname]["ip"]
 mapped_ns = node["ip_mapping"][region][hostname]["ns"]
 
-#eip_old=node["opsworks"]["instance"]["ip"]
-#ops_inst_id=node["opsworks"]["instance"]["id"]
-public_ip =eip_new
+hosted_zone_names = node["hosted_zones"]
 
+
+###############################
+#function
 =begin
-#get eip from instance
-opsworks = AWS::OpsWorks::Client.new
-resp = opsworks.describe_instances({instance_ids: [ops_inst_id]})
-instance=resp.instances[0]
-
-if instance.respond_to?(:hostname)
-    puts "### hostname: #{instance.hostname} ###"
+#not work
+def r53_hosted_zone_names2ids(hosted_zone_names, region_name)
+  r53 = AWS::Route53::Client.new(region: region_name)
+  hosted_zone_ids=[]
+  hosted_zone_names.each do |hosted_zone_name|
+    resp = r53.list_hosted_zones({marker: "PageMarker",max_items: 100})
+    if resp.respond_to?(:hosted_zones)
+      resp.hosted_zones do |hosted_zone|
+        puts "### hosted_zone name: #{hosted_zone.name} ###"
+        puts "### hosted_zone id: #{hosted_zone.id} ###"
+        if(hosted_zone.name == hosted_zone_name)
+          hosted_zone_ids.insert(-1, hosted_zone.id)
+        end
+      end
+    end
+  end
+  return hosted_zone_ids
 end
-if instance.respond_to?(:ec2_instance_id)
-    puts "### ec2_instance_id: #{instance.ec2_instance_id} ###"
-end
-if instance.respond_to?(:instance_id)
-    puts "### instance_id: #{instance.instance_id} ###"
-end
-
-if instance.respond_to?(:elastic_ip)
-    puts "### elastic_ip: #{instance.elastic_ip} ###"
-end
-
-if instance.respond_to?(:stack_id)
-    puts "### stack_id: #{instance.stack_id} ###"
-end
-
-if instance.respond_to?(:layer_ids)
-    instance.layer_ids.each { |layer_id| puts "### layer_id: #{layer_id} ###"}
-end
-
-public_ip=instance.elastic_ip
-puts "### get EIP from instance:#{public_ip} ###"
 =end
 
-#awstest.ers.trendmicro.com
-HostedZoneID='Z1QLIJZNNZ5SR0'
+def r53_cli_hosted_zone_names2ids(hosted_zone_names)
+  hosted_zone_ids=[]
+  hosted_zone_names.each do |hosted_zone_name|
+    hosted_zone_name_dot = "#{hosted_zone_name}."
+    #cmd = "aws route53 list-hosted-zones-by-name --dns-name awstest.ers.trendmicro.com"
+    cmd = "aws route53 list-hosted-zones-by-name --dns-name #{hosted_zone_name}"
+    #puts "### aws route53 list-hosted-zones-by-name ###"
+    value = %x( #{cmd} )
+    if value.include? '"HostedZones":'
+      zone_hash = JSON.parse(value)
+      if zone_hash.has_key?("HostedZones")
+        zone_hash["HostedZones"].each do |zonedata|
+          #puts "### HostedZones Id: #{zonedata["Id"]} ###"
+          #puts "### HostedZones Name: #{zonedata["Name"]} ###"
+          if zonedata["Name"] == hosted_zone_name_dot
+            #"zonedata["Id"]="/hostedzone/Z1QLIJZNNZ5SR0", 
+            hosted_zone_id_elements = zonedata["Id"].split('/')
+            hosted_zone_id = hosted_zone_id_elements[2]
+            hosted_zone_ids.insert(-1, hosted_zone_id)
+          end
+        end
+      end
+    end
+  end
+  return hosted_zone_ids
+end
 
-#aws.ers.trendmicro.com
-#HostedZoneID='Z3H87DJJHX9OQH'
+def r53_get_resource_record_sets(hostedzoneid, dnsname, type, set_identifier)
+  r53 = AWS::Route53::Client.new()
+  resp = r53.list_resource_record_sets({
+      hosted_zone_id: hostedzoneid,
+      start_record_name: dnsname,
+      start_record_type: type,
+      start_record_identifier: set_identifier,
+      max_items: 1})
+  res_records=[]
+  bNeedUpdate = true
+  bNeedCreate = true
+  resp.resource_record_sets.each do |record|
+     if record.respond_to?(:resource_records) and record.respond_to?(:set_identifier)
+        bNeedCreate = false
+        record.resource_records.each do |resource_record|
+           if resource_record.respond_to?(:value)
+              if resource_record.value == public_ip
+                  bNeedUpdate = false
+              else
+                  res_records.insert(-1,resource_record)
+              end
+              Chef::Log.info("##    value: #{resource_record.value}    ##")
+           end
+        end
+     end
+  end
+  return res_records,bNeedUpdate,bNeedCreate
+end
+
+def r53_change_resource_record_sets(hostedzoneid, bCreate, dnsname, 
+  rtype, rset_identifier, rregion, rttl, rrecords)
+
+  raction = bCreate ? "CREATE" : "UPSERT"
+  puts "### Action: #{raction} ###"
+  r53 = AWS::Route53::Client.new()
+  resp = r53.change_resource_record_sets({
+    hosted_zone_id: hostedzoneid,
+    change_batch: {
+      comment: "ResourceDescription",
+      changes: [
+        {
+          action: raction, # required, accepts CREATE, DELETE, UPSERT
+          resource_record_set: { # required
+            name: dnsname, # required
+            type: rtype, # required, accepts SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA
+            set_identifier: rset_identifier,
+            region: rregion, # accepts us-east-1, us-west-1, us-west-2, eu-west-1, eu-central-1, ap-southeast-1, ap-southeast-2, ap-northeast-1, sa-east-1, cn-north-1
+            ttl: rttl,
+            resource_records: rrecords
+          },
+        },
+      ],
+    },
+  })
+  Chef::Log.info("###   id: #{resp.change_info.id}   ###")
+  Chef::Log.info("###   status: #{resp.change_info.status}   ###")
+  Chef::Log.info("###   submitted_at: #{resp.change_info.submitted_at}   ###")
+  Chef::Log.info("###   comment: #{resp.change_info.comment}   ###")
+end
+###############################
 
 
-r53 = AWS::Route53::Client.new()
+#get hosted zone ids from names
+hosted_zone_ids=r53_cli_hosted_zone_names2ids(hosted_zone_names)
 
+#fill record date
 DNSName=mapped_ns #'ns1.awstest.ers.trendmicro.com.'
+DNSName_element=mapped_ns.split('.')
 record_type='A'
-record_set_identifier='awstestarq-ns1'
+record_set_identifier="arq-#{DNSName_element[1]}-#{DNSName_element[0]}" #arq-awstest-ns1
 record_region=region
 record_ttl=3600
 RData1=public_ip
-res_records=[{value: RData1}]
+record=[{value: RData1}]
 
 
-#try to get record set
+hosted_zone_ids.each do |hostedzoneid|
+  #try to get record set in each hosted zone
+  res_records,bNeedUpdate,bNeedCreate = r53_get_resource_record_sets(hostedzoneid, DNSName,
+                                          record_type, record_set_identifier)
+  res_records+=record
 
-resp = r53.list_resource_record_sets({
-    hosted_zone_id: HostedZoneID,
-    start_record_name: DNSName,
-    start_record_type: record_type,
-    start_record_identifier: record_set_identifier,
-    max_items: 1})
-
-bNeedUpdate = true
-bCreate = true
-resp.resource_record_sets.each do |record|
-   if record.respond_to?(:resource_records) and record.respond_to?(:set_identifier)
-      bCreate = false
-      record.resource_records.each do |resource_record|
-         if resource_record.respond_to?(:value)
-            if resource_record.value == public_ip
-                bNeedUpdate = false
-            else
-                res_records.insert(-1,resource_record)
-            end
-            Chef::Log.info("##    value: #{resource_record.value}    ##")
-         end
-      end
-   end
-end
-
-Action = bCreate ? "CREATE" : "UPSERT"
-puts "### Action: #{Action} ###"
-
-if bNeedUpdate
-    resp = r53.change_resource_record_sets({
-      hosted_zone_id: HostedZoneID,
-      change_batch: {
-        comment: "ResourceDescription",
-        changes: [
-          {
-            action: Action, # required, accepts CREATE, DELETE, UPSERT
-            resource_record_set: { # required
-              name: DNSName, # required
-              type: record_type, # required, accepts SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA
-              set_identifier: record_set_identifier,
-              region: record_region, # accepts us-east-1, us-west-1, us-west-2, eu-west-1, eu-central-1, ap-southeast-1, ap-southeast-2, ap-northeast-1, sa-east-1, cn-north-1
-              ttl: record_ttl,
-              resource_records: res_records
-            },
-          },
-        ],
-      },
-    })
+  if bNeedUpdate
     Chef::Log.info("###   Try to add: #{public_ip}   ###")
-    Chef::Log.info("###   id: #{resp.change_info.id}   ###")
-    Chef::Log.info("###   status: #{resp.change_info.status}   ###")
-    Chef::Log.info("###   submitted_at: #{resp.change_info.submitted_at}   ###")
-    Chef::Log.info("###   comment: #{resp.change_info.comment}   ###")
-else
-    Chef::Log.info("###   #{public_ip} already in record  ###")
+    #try to change record set in each hosted zone
+    r53_change_resource_record_sets(hostedzoneid, bNeedCreate, DNSName, 
+                record_type, record_set_identifier, record_region, record_ttl, res_records)
+  else
+      Chef::Log.info("###   #{public_ip} already in record  ###")
+  end
 end
--- ARQ3/recipes/opsworks_eip.rb
+++ ARQ3/recipes/opsworks_eip.rb
@@ -6,7 +6,7 @@ hostname = node["opsworks"]["instance"]["hostname"]
 
 eip_new = node["ip_mapping"][region][hostname]["ip"]
 mapped_ns = node["ip_mapping"][region][hostname]["ns"]
-eip_old=node["opsworks"]["instance"]["ip"]
+#eip_old=node["opsworks"]["instance"]["ip"]
 OpsWorks_instance_id=node["opsworks"]["instance"]["id"]
 
 
@@ -39,33 +39,39 @@ end
 
 opsworks = AWS::OpsWorks::Client.new
 
-#get stack_id from instance
+#get stack_id and public_ip from instance
 resp = opsworks.describe_instances({instance_ids: [OpsWorks_instance_id]})
 instance=resp.instances[0]
 stackid=instance.stack_id
+eip_old=instance.public_ip
 
-
-#regist eip to stack
-resp = opsworks.register_elastic_ip({elastic_ip: eip_new,stack_id: stackid})
-
-#associate eip
-resp = opsworks.associate_elastic_ip({elastic_ip: eip_new,instance_id: OpsWorks_instance_id})
-
-
-#deregist old eip
-resp = opsworks.deregister_elastic_ip({elastic_ip: eip_old})
-
-#release old eip
-ec2 = AWS::EC2::Client.new
-
-#get allocation id
-allocationid=aws_eip2allocationid(region,eip_old)
-
-
-#release old eip
-if allocationid.nil?
-  puts "### cannot get allocation id ###"
+if eip_old == eip_new
+  eip_old = nil
+  puts "### #{eip_new} already bind to instance###"
 else
-  resp = ec2.release_address({allocation_id: allocationid})
+  #regist eip to stack
+  resp = opsworks.register_elastic_ip({elastic_ip: eip_new,stack_id: stackid})
+
+  #associate eip
+  resp = opsworks.associate_elastic_ip({elastic_ip: eip_new,instance_id: OpsWorks_instance_id})
 end
 
+if eip_old.nil?
+  puts "### no eip need to free###"
+else
+  #deregist old eip
+  resp = opsworks.deregister_elastic_ip({elastic_ip: eip_old})
+
+  #release old eip
+  ec2 = AWS::EC2::Client.new
+
+  #get allocation id
+  allocationid=aws_eip2allocationid(region,eip_old)
+
+  #release old eip
+  if allocationid.nil?
+    puts "### cannot get allocation id ###"
+  else
+    resp = ec2.release_address({allocation_id: allocationid})
+  end
+end
